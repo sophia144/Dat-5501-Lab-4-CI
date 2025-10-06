5b79dcdb1144504a51f9d37ed98a930f4d0cb3c7
sophia144 <piercesp@outlook.com>
2025-10-06 12:53:52 +0100

    Added scatter on the x axis

--- data_generation.py
+++ data_generation.py
@@ -5,7 +5,10 @@ import random
 
 def generate_data(slope_m, intercept_c):
     #generating x values
-    x_coordinates = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    x_coordinates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    for x_index in range(0, len(x_coordinates)):
+        x_coordinates[x_index] = x_coordinates[x_index] + random.uniform(-1, 1)
+    x_coordinates = np.array(x_coordinates)
 
     #generating random y values
     y_coordinates_list = []
